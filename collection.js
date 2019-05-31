410a6d2018f5b2475c794017088011114a36a8d3
zappz88 <31833538+zappz88@users.noreply.github.com>
2019-05-31 04:44:43 -0500

    Initial File Uploaded

---
+++ collection.js
@@ -0,0 +1,75 @@
+class Collection {
+
+    constructor() {
+        this.collection = [];
+        this.index = 0;
+    }
+
+    log() {
+        return console.log(this.collection);
+    }
+
+    push(value) {
+        return this.collection.push(value);
+    }
+
+    pushAll(...values) {
+        return this.collection.push(...values);
+    }
+
+    pop() {
+        return this.collection.pop();
+    }
+
+    shift() {
+        return this.collection.shift();
+    }
+
+    unshift(value) {
+        return this.collection.unshift(value);
+    }
+
+    unshiftAll(...values) {
+        return this.collection.unshift(...values);
+    }
+
+    remove(index) {
+        return this.collection.splice(index, 1);
+    }
+
+    add(index, value) {
+        return this.collection.splice(index, 0, value);
+    }
+
+    replace(index, value) {
+        return this.collection.splice(index, 1, value);
+    }
+
+    clear() {
+        this.collection.length = 0;
+    }
+
+    isEmpty() {
+        return this.collection.length === 0;
+    }
+
+    viewFirst() {
+        return this.collection[0];
+    }
+
+    viewLast() {
+        return this.collection[this.collection.length - 1];
+    }
+
+    next() {
+        this.index++;
+        this.index > this.collection.length - 1 ? this.index = 0 : this.index;
+        return this.collection[this.index];
+    }
+    
+    previous(){
+        this.index--;
+        this.index < 0 ? (this.index = this.collection.length-1) : this.index;
+        return this.collection[this.index];
+    }
+}
